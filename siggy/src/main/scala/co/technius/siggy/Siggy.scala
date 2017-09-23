76777a9fc2d9c83051b1059292b410d27ccdfb09
Bryan Tan <techniux@gmail.com>
2017-09-23 11:22:04 -0700

    Refactor Signature into separate file

--- siggy/src/main/scala/co/technius/siggy/Siggy.scala
+++ siggy/src/main/scala/co/technius/siggy/Siggy.scala
@@ -127,38 +127,3 @@ object Siggy {
       tparamsLenMatch && (sigMatch || sigWithEncMatch)
     }
 }
-
-/**
-  * Represents a method signature.
-  * @param prefix The enclosing class, trait, or object, followed by a "." or a "#".
-  * @param tparams Type parameters
-  * @param params A list of parameter names and types
-  * @param tpe  The return type
-  */
-case class Signature(
-  prefix: String,
-  name: String,
-  tparams: List[String],
-  paramLists: List[List[(String, String)]],
-  tpe: String,
-  property: Boolean = false) {
-
-  def enclosingName: Option[String] = {
-    val i = prefix.lastIndexOf(".")
-    // last character is '.' or '#', so we need to remove it
-    if (i == -1 && prefix.length > i + 1) {
-      if (prefix.isEmpty) None else Some(prefix.init)
-    } else {
-      Some(prefix.slice(i + 1, prefix.length - 1))
-    }
-  }
-
-  override def toString: String = {
-    val tparamStr = if (tparams.isEmpty) "" else tparams.mkString("[", ",", "]")
-    val paramStr =
-      if (paramLists.isEmpty && property) ""
-      else paramLists.map(pl =>
-        pl.map(p => p._1 + ": " + p._2).mkString("(", ", ", ")")).mkString
-    s"$prefix$name$tparamStr$paramStr: $tpe"
-  }
-}
