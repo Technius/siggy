05acaf0c536b3794559c4cbdcc96f27f4f669a76
Bryan Tan <techniux@gmail.com>
2017-10-01 17:51:57 -0700

    Update Scalameta version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbtcrossproject.CrossPlugin.autoImport._
 object Dependencies {
   lazy val scalaTest = Def.setting("org.scalatest" %%% "scalatest" % "3.0.3")
 
-  lazy val scalaMeta = Def.setting("org.scalameta" %%% "scalameta" % "2.0.0-RC1")
+  lazy val scalaMeta = Def.setting("org.scalameta" %%% "scalameta" % "2.0.1")
 
   lazy val fastParse = Def.setting("com.lihaoyi" %%% "fastparse" % "0.4.4")
 }
